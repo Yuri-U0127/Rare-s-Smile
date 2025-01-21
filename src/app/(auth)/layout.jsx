ddfbfc7c89056f3e5239c0accb3d68a72594d960
Yuri <engineer032511@gmail.com>
2025-01-22 00:17:49 +0900

    fourth commit.

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -1,10 +1,8 @@
 export default function AuthLayout({ children }) {
   return (
-    <div style={{ padding: '20px', backgroundColor: '#f7f9fc' }}>
-      <header style={{ marginBottom: '20px' }}>
-        <h1>Authentication Pages</h1>
-      </header>
-      <main>{children}</main>
-    </div>
+
+
+    <main>{children}</main>
+
   );
 }
